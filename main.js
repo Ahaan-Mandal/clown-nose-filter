90eef9fd7ffef3761936ba2895f731e9112706da
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-02-05 16:47:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+noseX=0;
+noseY=0;
+
+
 function preload() {
+clown_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 
 }
 
@@ -15,6 +20,10 @@ function setup() {
 
 function draw(){
 image(video,0,0,300,300);
+image(clown_nose,noseX,noseY,50,50);
+
+
+
 }
 
 function take_snapshot(){
@@ -28,7 +37,10 @@ function modelLoaded(){
 function gotPoses(results){
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x=" + results[0].pose.nose.x);
-        console.log("nose y=" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y-20;
+        console.log("nose x=" + noseX);
+        console.log("nose y=" + noseY);
+        
     }
 }
